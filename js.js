12de51b380a552dab2fd37da5700c66c71f80aa2
vothikimyen108 <vothikinyen2k@gmail.com>
2021-06-13 19:26:37 +0700

    code animate load

--- js.js
+++ js.js
@@ -1,13 +1,6 @@
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "body").style.visibility = "hidden";
-        document.querySelector(
-          "#loader").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#loader").style.display = "none";
-        document.querySelector(
-          "body").style.visibility = "visible";
-    }
-};
+
+  var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+  setTimeout(function () {
+    window.location="./Home.html";
+  }, 2000);
+
